cca62f31d3b2e129d08b481a3ee5e220279452b7
mojaalagevai <mojaalagevai@gmail.com>
2025-07-16 05:28:25 +0600

    Update variables.py

--- colab_leecher/utility/variables.py
+++ colab_leecher/utility/variables.py
@@ -71,20 +71,20 @@ class BotTimes:
 
 
 class Paths:
-    WORK_PATH = "Telegram-Leecher/BOT_WORK"
-    THMB_PATH = "Telegram-Leecher/colab_leecher/Thumbnail.jpg"
+    WORK_PATH = "BOT_WORK"
+    THMB_PATH = "colab_leecher/Thumbnail.jpg"
     VIDEO_FRAME = f"{WORK_PATH}/video_frame.jpg"
     HERO_IMAGE = f"{WORK_PATH}/Hero.jpg"
-    DEFAULT_HERO =  "/content/Telegram-Leecher/custom_thmb.jpg"
-    MOUNTED_DRIVE = "/content/drive"
+    DEFAULT_HERO =  "custom_thmb.jpg"
+    MOUNTED_DRIVE = "drive"
     down_path = f"{WORK_PATH}/Downloads"
     temp_dirleech_path = f"{WORK_PATH}/dir_leech_temp"
-    mirror_dir = "/content/drive/MyDrive/Colab Leecher Uploads"
+    mirror_dir = "dl"
     temp_zpath = f"{WORK_PATH}/Leeched_Files"
     temp_unzip_path = f"{WORK_PATH}/Unzipped_Files"
     temp_files_dir = f"{WORK_PATH}/leech_temp"
     thumbnail_ytdl = f"{WORK_PATH}/ytdl_thumbnails"
-    access_token = "/content/token.pickle"
+    access_token = "token.pickle"
 
 
 class Messages:
